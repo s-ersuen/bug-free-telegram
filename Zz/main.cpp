f9d8f8cbea2d939ee4ef76cede88981c11d86454
Eric Sündermann <s-ersuen@frontend.service>
2019-06-19 13:38:39 +0200

    ...

--- Zz/main.cpp
+++ Zz/main.cpp
@@ -1,19 +1,27 @@
 #include <random> 
 #include <chrono> 
 #include <iostream>
+#include<fstream>
 using namespace std;
 int main()
 {
+  ofstream out("output.txt");
   mt19937 gen;
-  gen.seed(213);
-     
-  vector<int> my_vector(10,0);
-  uniform_int_distribution<int> dis(0, my_vector.size()-1);
-  for (int n=0; n< 10; ++n){
-    my_vector[dis(gen)]++;
+  gen.seed(5);
+   int dt=100;
+   
+   int p=100; 
+  uniform_int_distribution<int> dis(0,1);
+  for (int m=0; m<p ; ++m){
+    int x=0;
+  for (int n=0; n<dt ; ++n){
+    int y=dis((gen)); 
+    if (y==1) {x=x+1 ; }
+    else {x=x-1 ;}
   }
-  for(int e:my_vector){
-    cout << e << endl;}
+  x=x/100;
+out << x << endl;
+  }
+  }  
     
-    
-}
+
