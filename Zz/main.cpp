80d3d096c9368973ab9c526b234acab7114ca424
Eric Sündermann <s-ersuen@frontend.service>
2019-06-19 14:03:31 +0200

    a,b

--- Zz/main.cpp
+++ Zz/main.cpp
@@ -8,20 +8,23 @@ int main()
   ofstream out("output.txt");
   mt19937 gen;
   gen.seed(5);
-   int dt=100;
-   
-   int p=100; 
+   int p=10000;
+  double  x=0; 
+   int dt=10; 
   uniform_int_distribution<int> dis(0,1);
-  for (int m=0; m<p ; ++m){
-    int x=0;
-  for (int n=0; n<dt ; ++n){
+  for (int m=0; m<dt ; ++m){
+    
+  for (int n=0; n<p ; ++n){
     int y=dis((gen)); 
     if (y==1) {x=x+1 ; }
     else {x=x-1 ;}
+
   }
-  x=x/100;
-out << x << endl;
+  x=x*x/p;
+    out<<x<<endl;
+    cout<<x<<endl;
   }
+
   }  
     
 
