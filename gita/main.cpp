c06c91082796d03dbd2515c834552fc28ebb9d1d
ich <suendermanneric@gmail.com>
2019-07-03 16:31:44 +0200

    Erste versuche...

--- gita/main.cpp
+++ gita/main.cpp
@@ -4,11 +4,17 @@
 #imclude <array>
 int main(){
 array<array<array<double,100>,100>,100> gitter;
-			for (int i=0 ; i<gitter.size();++i){
+int tges=10;
+vector <double> phi0(tges+1,0);
+vectro <double> phi1(tges+1,0);
+for(int t=0, t<tges , t++){
+	for (int i=0 ; i<gitter.size();++i){
  		for (int j=0; j<gitter.size(); ++j){
-	for (int k=0; k<gitter.size(); ++k){
-if (i==0){gitter[i][j][k]=1000;} else {gitter[i][j][k]=0;}
-			}  
-		}
- 	}
+			for (int k=0; k<gitter.size(); ++k){
+if (i==0){gitter[i][j][k]=1000;} else {gitter[i][j][k]=0; }
+
+}
+	}
+ 		}
+			}
 }
