418cde62fe79565722f55443bee195f68509b125
Eric Sündermann <s-ersuen@frontend.service>
2019-07-13 17:54:59 +0200

    final

--- gita/main.cpp
+++ gita/main.cpp
@@ -1,20 +1,60 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-#imclude <array>
+#include <array>
+using namespace std;
 int main(){
+int a=100;
+ofstream out("output.txt");
+int tges = 20;
 array<array<array<double,100>,100>,100> gitter;
-int tges=10;
-vector <double> phi0(tges+1,0);
-vectro <double> phi1(tges+1,0);
-for(int t=0, t<tges , t++){
-	for (int i=0 ; i<gitter.size();++i){
- 		for (int j=0; j<gitter.size(); ++j){
-			for (int k=0; k<gitter.size(); ++k){
-if (i==0){gitter[i][j][k]=1000;} else {gitter[i][j][k]=0; }
+array<array<array<double,100>,100>,100> gitter2;
+array<array<array<double,100>,100>,100> gitter3;
 
+//	for (int i=0 ; i<a;++i){
+// 		for (int j=0; j<a; ++j){
+//			for (int k=0; k<a; ++k){
+//if (i==0){gitter[i][j][k]=1000.;} else {gitter[i][j][k]=0; }
+
+//}
+//	}
+ //		}
+//for(int t=0; t<tges; t++) {
+// for(int i=1 ; i<a -1 ; i++){
+ // for(int j=1; j<a -1; ++j){
+  // for(int k=1; k<a -1; ++k){
+ // gitter[i][j][k]= (1./6.) *( gitter[i+1][j][k]+ gitter[i-1][j][k]+ gitter[i][j+1][k]+ gitter[i][j-1][k]+ gitter[i][j][k+1]+ gitter[i][j][k-1]);
+ //gitter2[i][j][k]=gitter[i][j][k];
+ // }
+ // }         
+// }
+//}
+//for ( int i =0 ; i<a ; i++){
+//for (int k=0; k<a ; k++){
+//out << i << " " << k << " " << gitter2[i][50][k]<<endl;
+//}
+//}
+	for (int i=0 ; i<a;++i){
+ 		for (int j=0; j<a; ++j){
+			for (int k=0; k<a; ++k){
+gitter[i][j][k]=0.;
+gitter3[i][j][k]=0.;
+gitter[20][20][20]=100.;
+gitter3[20][20][20]=100.;
+}}}
+for (int t=0; t<2000; ++t){ 
+for (int i=1; i<a-1 ; i++){ 
+for (int j=1; j<a-1; j++){
+for (int k=1;k<a-1;k++){
+gitter[i][j][k]= (1./6.) *( gitter[i+1][j][k]+ gitter[i-1][j][k]+ gitter[i][j+1][k]+ gitter[i][j-1][k]+ gitter[i][j][k+1]+ gitter[i][j][k-1]-gitter3[i][j][k]);
+gitter2[i][j][k]=gitter[i][j][k];
+}}}}
+
+
+for (int i=0; i<a; i++){
+for (int k=0; k<a;k++){
+out << i << " " << k << " " << gitter2[i][50][k] << endl;
 }
-	}
- 		}
-			}
 }
+}
+
